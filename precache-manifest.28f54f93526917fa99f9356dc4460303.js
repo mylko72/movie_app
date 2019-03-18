52025d93bebb980cb87334324a0a1fafcccf672b
mylko <mypmk@naver.com>
2019-03-18 20:23:04 +0900

    Updates

---
+++ precache-manifest.28f54f93526917fa99f9356dc4460303.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "18ff3c968853b792c692",
+    "url": "/movie_app/static/js/main.a7f58d4b.chunk.js"
+  },
+  {
+    "revision": "df0b9ca0f81249544de6",
+    "url": "/movie_app/static/js/2.1267fdac.chunk.js"
+  },
+  {
+    "revision": "18ff3c968853b792c692",
+    "url": "/movie_app/static/css/main.08666f36.chunk.css"
+  },
+  {
+    "revision": "bfd279df8fe190a70e98efdda3c6bac6",
+    "url": "/movie_app/index.html"
+  }
+];
